c017c31d1d116e12af92dce6dc838819e2c167d2
MarcoSterlini <s266662@ocsb.local>
2018-10-01 14:51:48 -0400

    added images

--- MovingImagesMarcoS/main.lua
+++ MovingImagesMarcoS/main.lua
@@ -1,7 +1,36 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: MovingImages
+-- Name: Marco Sterlini
+-- Course: ICS2O
+-- This program displays an image that moves across the screen
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- global varibles 
+scrollSpeed = 3
+
+-- background image with width and height
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+ 
+-- character image with width and height
+local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+
+-- set the image to be transparent
+beetleship.alpha = 0
+
+-- set the inital x and y position of the beetleship
+beetleship.x = 0
+beetleship.y = display.contentHeight/3
+
+-- Function: MoveShip
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This functuion adds thye scroll speed to the x value of the ship
+local function MoveShip(event)
+	-- add the scroll speed to the x value of the ship
+	beetleship.x = beetleship.x + scrollSpeed
+	-- change the transparency of thye ship every time it moves so that it fades out
+    beetleship.alpha = beetleship.alpha + 0.01
+end
+-- moveShip will be called over and over again 
+Runtime:addEventListener("enterFrame", MoveShip)
