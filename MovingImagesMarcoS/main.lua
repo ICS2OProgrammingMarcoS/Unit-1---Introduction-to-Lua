2e9b9b68f5e2f5824b9bdee9d79f74251a7ee9f2
MarcoSterlini <s266662@ocsb.local>
2018-10-03 13:53:15 -0400

    Added touch and drag

--- MovingImagesMarcoS/main.lua
+++ MovingImagesMarcoS/main.lua
@@ -8,20 +8,24 @@ display.setStatusBar(display.HiddenStatusBar)
 
 -- global varibles 
 scrollSpeed = 3
-
+scrollSpeed2 = 4
 -- background image with width and height
 local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
  
 -- character image with width and height
 local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
 
+local rocketship = display.newImageRect("Images/rocketship.png", 200, 200)
 -- set the image to be transparent
 beetleship.alpha = 0
 
+rocketship.alpha = 1
 -- set the inital x and y position of the beetleship
 beetleship.x = 0
 beetleship.y = display.contentHeight/3
 
+rocketship.x = 1024
+rocketship.y = display.contentHeight*2/3
 -- Function: MoveShip
 -- Input: this function accepts an event listener
 -- Output: none
@@ -34,3 +38,15 @@ local function MoveShip(event)
 end
 -- moveShip will be called over and over again 
 Runtime:addEventListener("enterFrame", MoveShip)
+
+local function MoveRocketship(event)
+	rocketship.x = rocketship.x - scrollSpeed2
+	rocketship.alpha = rocketship.alpha - 0.001
+end
+
+Runtime:addEventListener("enterFrame", MoveRocketship)
+ 
+rocketship.xScale = -1
+
+transition.to(rocketship, {x=-250, rotation = rocketship.rotation-360,time=2000,} )
+
