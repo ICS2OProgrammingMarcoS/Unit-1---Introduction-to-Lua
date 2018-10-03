93686becc86c6e80a5f916217713abdeb1a0d1da
MarcoSterlini <s266662@ocsb.local>
2018-10-03 15:36:26 -0400

    added touch and drag

--- MovingImagesMarcoS/main.lua
+++ MovingImagesMarcoS/main.lua
@@ -14,18 +14,22 @@ local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536
  
 -- character image with width and height
 local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
-
+-- character image with width and height
 local rocketship = display.newImageRect("Images/rocketship.png", 200, 200)
 -- set the image to be transparent
 beetleship.alpha = 0
-
+-- set image to apear 
 rocketship.alpha = 1
 -- set the inital x and y position of the beetleship
 beetleship.x = 0
 beetleship.y = display.contentHeight/3
 
+-- set the inital x and y position of the rocketship
 rocketship.x = 1024
 rocketship.y = display.contentHeight*2/3
+
+-- make it point to the left
+rocketship.xScale = -1
 -- Function: MoveShip
 -- Input: this function accepts an event listener
 -- Output: none
@@ -35,18 +39,20 @@ local function MoveShip(event)
 	beetleship.x = beetleship.x + scrollSpeed
 	-- change the transparency of thye ship every time it moves so that it fades out
     beetleship.alpha = beetleship.alpha + 0.01
-end
+    	beetleship:scale(1.002, 1.002)
+    end
 -- moveShip will be called over and over again 
 Runtime:addEventListener("enterFrame", MoveShip)
-
+-- Move ship
 local function MoveRocketship(event)
 	rocketship.x = rocketship.x - scrollSpeed2
 	rocketship.alpha = rocketship.alpha - 0.001
+	rocketship:scale(0.99777, 0.99777)
 end
-
+-- moveRocketship will be called over and over again
 Runtime:addEventListener("enterFrame", MoveRocketship)
  
-rocketship.xScale = -1
 
-transition.to(rocketship, {x=-250, rotation = rocketship.rotation-360,time=2000,} )
+
+
 
--- TouchAndDragMarcoS/main.lua
+++ TouchAndDragMarcoS/main.lua
@@ -1,7 +1,75 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: TouchAndDrag
+-- Name: Marco Sterlini
+-- Course: ICS2O
+-- This Program displays images that react to a person's finger
 
--- Your code here
+-- Hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- local varibles. 
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+local yellowGirl = display.newImageRect("Images/girl1.png", 150, 150)
+local yellowGirlWidth = yellowGirl.width
+local yellowGirlHeight = yellowGirl.height
+
+local blueGirl = display.newImageRect("Images/girl5.png", 150, 150)
+local blueGirlWidth = blueGirl.width
+local blueGirlHeight = blueGirl.height
+
+-- my boolean varibles to keep track of which object I touched first
+local alreadyTouchedYellowGirl = false
+local alreadyTouchedBlueGirl = false
+
+-- set the initial x and y position of myImage
+yellowGirl.x = 400
+yellowGirl.y = 500
+
+blueGirl.x = 300
+blueGirl.y = 200
+
+-- function: BlueGirlListaner
+-- Input: touch listaner
+-- Output: None
+-- Description: when Blus girl is touched, move her
+local function BlueGirlListener(touch)
+
+  if (touch.phase == "began") then
+	    if (alreadyTouchedYellowGirl == false) then
+		    alreadyTouchedBlueGirl = true
+	    end
+    end
+
+    if( (touch.phase == "moved") and (alreadyTouchedBlueGirl == true) ) then
+	 	blueGirl.x = touch.x
+	 	blueGirl.y = touch.y
+	end
+
+ 
+    if (touch.phase == "ended") then
+        alreadyTouchedBlueGirl = false
+        alreadyTouchedYellowGirl = false
+    end
+end
+
+local function YellowGirlListener(touch)
+
+   if (touch.phase == "began") then
+	    if (alreadyTouchedBlueGirl == false) then
+		    alreadyTouchedYellowGirl = true
+	    end
+    end
+
+	if( (touch.phase == "moved") and (alreadyTouchedYellowGirl == true) ) then
+	 	yellowGirl.x = touch.x
+	 	yellowGirl.y = touch.y
+    end
+ 
+    if (touch.phase == "ended") then
+        alreadyTouchedYellowGirl = false
+        alreadyTouchedBlueGirl = false
+    end
+end
+-- add the respective listaners to each object
+blueGirl:addEventListener("touch", BlueGirlListener)
+
+yellowGirl:addEventListener("touch", YellowGirlListener)
